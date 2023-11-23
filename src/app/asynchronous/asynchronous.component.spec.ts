3c80e600bc5200060f9102ba4ec04235e9776076
Mohan91002 <mohankumarsatyavarapu225@gmail.com>
2023-11-23 21:32:52 +0530

    async component generated

---
+++ src/app/asynchronous/asynchronous.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AsynchronousComponent } from './asynchronous.component';
+
+describe('AsynchronousComponent', () => {
+  let component: AsynchronousComponent;
+  let fixture: ComponentFixture<AsynchronousComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AsynchronousComponent]
+    });
+    fixture = TestBed.createComponent(AsynchronousComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
